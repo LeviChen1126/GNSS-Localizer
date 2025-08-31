0ce9b0c817c04fe37912d36138883fcc29499cbd
Levi Chen <71495864+LeviChen1126@users.noreply.github.com>
2025-09-01 01:05:51 +0800

    Update fix2tfpose.cpp

--- gnss_localizer/fix2tfpose/fix2tfpose.cpp
+++ gnss_localizer/fix2tfpose/fix2tfpose.cpp
@@ -36,24 +36,16 @@ static double yaw;
 
 static int _plane;
 
-double fix2_lat, fix2_lon, fix2_alt;
-
 static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
 {
   geo_pos_conv geo;
-  geo_pos_conv geo2;
 
   geo.set_plane(_plane);
   geo.llh_to_xyz(msg->latitude, msg->longitude, msg->altitude);
-  geo2.set_plane(_plane);
-  geo2.llh_to_xyz(fix2_lat, fix2_lon, fix2_alt);
 
   static tf::TransformBroadcaster pose_broadcaster;
   tf::Transform pose_transform;
   tf::Quaternion pose_q;
-  static tf::TransformBroadcaster pose_broadcaster2;
-  tf::Transform pose_transform2;
-  tf::Quaternion pose_q2;
 
   geometry_msgs::PoseStamped pose;
   pose.header = msg->header;
@@ -62,14 +54,6 @@ static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
   pose.pose.position.x = geo.y();
   pose.pose.position.y = geo.x();
   pose.pose.position.z = geo.z();
-
-  geometry_msgs::PoseStamped fix2;
-  fix2.header = msg->header;
-  // pose.header.stamp = ros::Time::now();
-  fix2.header.frame_id = "map";
-  fix2.pose.position.x = geo2.y();
-  fix2.pose.position.y = geo2.x();
-  fix2.pose.position.z = geo2.z();
 
   // set gnss_stat
   if (pose.pose.position.x == 0.0 || pose.pose.position.y == 0.0 || pose.pose.position.z == 0.0)
@@ -85,25 +69,16 @@ static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
                          pow(pose.pose.position.x - _prev_pose.pose.position.x, 2));
   std::cout << "distance : " << distance << std::endl;
 
-  // if (distance > 0.2)
-  // {
-  //   yaw = atan2(pose.pose.position.y - _prev_pose.pose.position.y, pose.pose.position.x - _prev_pose.pose.position.x);
-  //   _quat = tf::createQuaternionMsgFromYaw(yaw);
-  //   _prev_pose = pose;
-  // }
-
-  yaw = atan2(fix2.pose.position.y - pose.pose.position.y, fix2.pose.position.x - pose.pose.position.x);
-  _quat = tf::createQuaternionMsgFromYaw(yaw);
+  if (distance > 0.2)
+  {
+    yaw = atan2(pose.pose.position.y - _prev_pose.pose.position.y, pose.pose.position.x - _prev_pose.pose.position.x);
+    _quat = tf::createQuaternionMsgFromYaw(yaw);
+    _prev_pose = pose;
+  }
 
   pose.pose.orientation = _quat;
   pose_publisher.publish(pose);
   stat_publisher.publish(gnss_stat_msg);
-  fix2.pose.orientation = _quat;
-
-  //test
-  // pose.header.stamp = ros::Time::now();
-  test_pub.publish(fix2);
-
 
   //座標変換
   static tf::TransformBroadcaster br;
@@ -113,22 +88,6 @@ static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
   q.setRPY(0, 0, yaw);
   transform.setRotation(q);
   br.sendTransform(tf::StampedTransform(transform, msg->header.stamp, "map", "gps_back"));
-
-  //座標変換
-  static tf::TransformBroadcaster br2;
-  tf::Transform transform2;
-  tf::Quaternion q2;
-  transform2.setOrigin(tf::Vector3(fix2.pose.position.x, fix2.pose.position.y, fix2.pose.position.z));
-  q2.setRPY(0, 0, yaw);
-  transform2.setRotation(q2);
-  br2.sendTransform(tf::StampedTransform(transform2, msg->header.stamp, "map", "gps_front"));
-}
-
-static void fix_2Callback(const sensor_msgs::NavSatFixConstPtr &msg)
-{
-  fix2_lat = msg->latitude;
-  fix2_lon = msg->longitude;
-  fix2_alt = msg->altitude;
 }
 
 int main(int argc, char **argv)
@@ -140,7 +99,6 @@ int main(int argc, char **argv)
   pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("gnss_pose", 1000);
   stat_publisher = nh.advertise<std_msgs::Bool>("/gnss_stat", 1000);
   ros::Subscriber gnss_pose_subscriber = nh.subscribe("fix", 100, GNSSCallback);
-  ros::Subscriber test = nh.subscribe("fix_2", 100, fix_2Callback);
 
   test_pub = nh.advertise<geometry_msgs::PoseStamped>("test_pose", 1000);
 
